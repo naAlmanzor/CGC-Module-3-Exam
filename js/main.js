ddd4a4a028b242e15b319b5a8a06aa54c12de554
naAlmanzor <neilalmanzor28@gmail.com>
2022-01-30 15:42:35 +0800

    Added files

---
+++ js/main.js
@@ -0,0 +1,16 @@
+import * as THREE from './three.module.js';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  10,
+  1000
+);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+console.log('test');
+
+renderer.render(scene, camera);
