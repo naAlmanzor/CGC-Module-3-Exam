24f95830221f9c61b5421a877092787c0bb4c4c2
naAlmanzor <neilalmanzor28@gmail.com>
2022-01-31 09:59:06 +0800

    Added stairs

--- js/main.js
+++ js/main.js
@@ -31,16 +31,16 @@ console.log('test');
 createRoom();
 lighting();
 
-function wall(width, height, depth){
-    const wall = new THREE.Mesh(
-        new THREE.BoxBufferGeometry(width, height, depth), 
-        new THREE.MeshLambertMaterial({color: 0xfCCCCCC})
-    )
-
-    return wall;
-}
-
 function createWalls(){
+    
+    function wall(width, height, depth){
+        const wall = new THREE.Mesh(
+            new THREE.BoxBufferGeometry(width, height, depth), 
+            new THREE.MeshLambertMaterial({color: 0xfCCCCCC})
+        )
+    
+        return wall;
+    }
 
     const walls = new THREE.Group;
     
@@ -60,8 +60,8 @@ function createWalls(){
     leftWallBack.position.z = -24;
     leftWallSide.add(leftWallBack);
 
-    const leftWallBtm = new wall(10, 10, 60);
-    leftWallBtm.position.y = -24.8;
+    const leftWallBtm = new wall(10, 15, 60);
+    leftWallBtm.position.y = -23;
     leftWallSide.add(leftWallBtm);
 
     const leftWallBridge = new wall(2, 2, 60);
@@ -84,6 +84,107 @@ function createWalls(){
 
 }
 
+function createStairs(){
+    
+    function creatStair(width, height, depth){
+        const stair = new THREE.Mesh(
+            new THREE.BoxBufferGeometry(width, height, depth),
+            new THREE.MeshLambertMaterial({color: 0xBCB0A0})
+        )
+        
+        return stair
+    }
+
+    const stairCase = new THREE.Group;
+    
+    const stairsPad = new creatStair(75, 6, 10)
+    stairsPad.position.x = 20;
+    stairsPad.position.y = 3.1;
+    stairCase.add(stairsPad)
+
+    const longStair = new creatStair(36, 24.2, 10)
+    longStair.position.x = 39.2;
+    longStair.position.y = 18.4;
+    stairCase.add(longStair)
+
+    const stairHandle = new creatStair(26, 0.69, 1)
+    stairHandle.rotation.z = 6.9;
+    stairHandle.position.x = 3;
+    stairHandle.position.y = 28;
+    stairCase.add(stairHandle)
+
+
+    function createStairSize(sHeight, lHeight){
+        const sStair = new THREE.Group;
+        const stairBuildS = new creatStair(4, sHeight, 10)
+        sStair.add(stairBuildS)
+    
+        const stairBuildL = new creatStair(6, lHeight, 10)
+        stairBuildL.position.x = -2;
+        stairBuildL.position.y = -1.6;
+        sStair.add(stairBuildL)
+
+        return sStair;
+    }
+
+    const sStairBtm = new createStairSize(6, 3);
+    sStairBtm.position.set(-12.5, 3.1, 7);
+    sStairBtm.rotation.y = -4.71;
+    stairCase.add(sStairBtm);
+
+    const sStairTop = new createStairSize(6, 3);
+    sStairTop.position.set(-2.6, 9.3, 0);
+    stairCase.add(sStairTop);
+
+    const mStairTop = new createStairSize(12.2, 9);
+    mStairTop.position.x = 4.5;
+    mStairTop.position.y = 12.4;
+    stairCase.add(mStairTop);
+
+    const lStairTop = new createStairSize(18.2, 15);
+    lStairTop.position.x = 11.7;
+    lStairTop.position.y = 15.4;
+    stairCase.add(lStairTop);
+
+    const xlStairTop = new createStairSize(24.2, 21);
+    xlStairTop.position.x = 18.91;
+    xlStairTop.position.y = 18.4;
+    stairCase.add(xlStairTop);
+
+    function createStairGap(width, height, depth){
+    
+        const stairGap = new THREE.Mesh(new THREE.BoxBufferGeometry(width, height, depth),new THREE.MeshLambertMaterial({color: 0x574a3a}))
+
+        return stairGap;
+    }
+
+    const sStairGap = new createStairGap(0.2, 6, 10)
+    sStairGap.position.x = -0.5
+    sStairGap.position.y = 9.3
+    stairCase.add(sStairGap);
+
+    const mStairGap = new createStairGap(0.2, 12.2, 10)
+    mStairGap.position.x = 6.6
+    mStairGap.position.y = 12.4
+    stairCase.add(mStairGap);
+
+    const lStairGap = new createStairGap(0.2, 18.2, 10)
+    lStairGap.position.x = 13.8
+    lStairGap.position.y = 15.4
+    stairCase.add(lStairGap);
+
+    const xlStairGap = new createStairGap(0.2, 24.2, 10)
+    xlStairGap.position.x = 21
+    xlStairGap.position.y = 18.4
+    stairCase.add(xlStairGap);
+
+    const stairPadGap = new createStairGap(66, 0.2, 10)
+    stairPadGap.position.x = 25.4
+    stairPadGap.position.y = 6.2
+    stairCase.add(stairPadGap);
+
+    return stairCase;
+}
 
 
 function createRoom(){
@@ -114,7 +215,10 @@ function createRoom(){
     floor.position.x = 10;
     room.add(floor);
 
-
+    const stairs = new createStairs();
+    stairs.position.z = -25
+    stairs.position.y = 0.09;
+    room.add(stairs);
 
     room.position.y = -20;
     scene.add(room);
